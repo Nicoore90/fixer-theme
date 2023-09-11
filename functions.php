f4026fcabf4e4422ca85562aa38eebad8bd5f6e6
Nicolas Orecchia <nicolas.orecchia@somosforma.com>
2023-09-11 20:45:55 -0300

    boton de whatsapp

--- functions.php
+++ functions.php
@@ -9,3 +9,11 @@ function druco_child_register_scripts(){
     );
 }
 add_action( 'wp_enqueue_scripts', 'druco_child_register_scripts', 99 );
+
+add_action('wp_footer', function() {
+    ?>
+
+    <a href='https://wa.me/543492643820' class="whatsapp-button"><i class="fa-brands fa-whatsapp"></i></a>
+
+    <?php
+});
