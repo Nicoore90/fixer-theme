3693ab5bcbb0aa27c521b3dba874c73f653c798b
Nicolas Orecchia <nicolas.orecchia@somosforma.com>
2023-09-16 11:18:27 -0300

    arreglo boton whatsapp

--- functions.php
+++ functions.php
@@ -7,13 +7,16 @@ function druco_child_register_scripts(){
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
+    
 }
 add_action( 'wp_enqueue_scripts', 'druco_child_register_scripts', 99 );
 
 add_action('wp_footer', function() {
     ?>
 
-    <a href='https://wa.me/543492643820' class="whatsapp-button"><i class="fa-brands fa-whatsapp"></i></a>
+    
+<a href='https://wa.me/543492643820' class="whatsapp-button"><i class="fa-brands fa-whatsapp"></i></a>
+
 
     <?php
 });
